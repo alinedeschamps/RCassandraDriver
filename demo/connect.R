01f0674840dbbe7acffd41641b1ab07867d14a69
Cédric M <cmillauriaux@icysoft.fr>
2015-10-05 00:17:38 +0200

    Ajout du chargement automatique des librairies dynamiques

--- demo/connect.R
+++ demo/connect.R
@@ -1,14 +1,5 @@
-source("R/RCassandraDriver.R")
-source("R/setColClasses.R")
-dyn.load('src/x64/cassandra.dll')
-dyn.load('src/x64/libuv.dll')
-dyn.load('src/RCassandraDriver.dll')
+library(RCassandraDriver)
 
 connectWithCredentials("94.23.20.120", "analytics", "s5dxs4c9")
 result <- executeQuery("SELECT * FROM icypricescrawler.products LIMIT 500")
-
 disconnect()
-
-dyn.unload('src/x64/cassandra.dll')
-dyn.unload('src/x64/libuv.dll')
-dyn.unload('src/RCassandraDriver.dll')
